e9d5c7f33b74ffb090f1c60f98e2bbd4e726eb98
Noshin Rahman <nr0869031@student.francistuttle.edu>
2020-11-02 15:10:26 -0600

    major update

---
+++ designnewbie.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+<div class="main">
+    <h1>Francis Tuttle Portfolio</h1>
+    <div class="ftsubgrid">
+        <div class="ftnav">
+            <?php include 'ftnav.php'; ?>
+        </div>
+        <div class="fthw">
+            <h2>Design Newbie</h2>
+            <h3>Responsive Typography Techniques</h3>
+            <P><a href="assets/port/ft/designnewbie/responsivetypography/responsivetypography.pdf" target="_blank">Unit Notes</a></P>
+            <p>Chapter 2: <a href="assets/port/ft/designnewbie/responsivetypography/ch02/index.html" target="_blank">Establishing Rhythm in Your Typographic System</a></p>
+            <p>Chapter 3: <a href="assets/port/ft/designnewbie/responsivetypography/ch03/index.html" target="_blank">Creating Type Prototype Pages</a></p>
+            <p>Chapter 4: <a href="assets/port/ft/designnewbie/responsivetypography/ch04/index.html" target="_blank">Setting Type for Readability Across Screen Sizes</a></p>
+            
+            <h3>Photoshop for Web Design</h3>
+            <p><a href="assets/port/ft/designnewbie/psforwd/psforwd.pdf" target="_blank">Unit Notes</a></p>
+            
+            <h3>Graphic Design Foundations</h3>
+            <p><a href="assets/port/ft/designnewbie/gdfoundations/gdfoundations.pdf" target="_blank">Unit Notes</a></p>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
--- footer.php
+++ footer.php
@@ -4,6 +4,24 @@ echo '
     <footer>
         &copy; 2020 Noshin Rahman
     </footer>
+    
+    <!-- Default Statcounter code for Noshinr.com
+http://noshinr.com -->
+<script type="text/javascript">
+var sc_project=10410292; 
+var sc_invisible=1; 
+var sc_security="b9037704"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="https://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="https://c.statcounter.com/10410292/0/b9037704/1/"
+alt="Web Analytics"></a></div></noscript>
+<!-- End of Statcounter Code -->
+    
 </body>
 </html>
 '
